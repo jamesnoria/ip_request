bf3d3c6285835455790223685769ab104e1a31d6
James Noria <james.noriavelarde@tcs.com>
2023-04-03 11:11:14 -0500

    feat: new way

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require('express');
 const app = express();
 const useragent = require('useragent');
 const requestIp = require('request-ip');
+const UAParser = require('ua-parser-js');
+
+app.get('/test', function (req, res) {
+  const parser = new UAParser();
+  const result = parser.setUA(req.headers['user-agent']).getResult();
+  const brand = result.device.vendor;
+  const model = result.device.model;
+  res.send(`Brand: ${brand}, Model: ${model}`);
+});
 
 app.get('/', (req, res) => {
   const source = req.headers['user-agent'];
@@ -14,6 +23,8 @@ app.get('/', (req, res) => {
   const device = agent.device.family;
   const brand = agent.device.brand;
   const model = agent.device.model;
+
+  console.log(agent.device)
 
   res.send({
     brw_navegador: browser,
